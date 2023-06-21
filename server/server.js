4dcc910d3be43b21548ffe3f8dce1b083052bb86
ACF <alricfernandes11@gmail.com>
2023-06-21 16:57:24 -0400

    Adding server port

--- server/server.js
+++ server/server.js
@@ -2,9 +2,14 @@
 const express = require('express');
 const cors = require('cors');
 
-// Global variables
+// Configuration
 const app = express();
+const PORT = 8000;
 
 // Middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`)
+})
