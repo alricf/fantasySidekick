c5081780511e44f6e04ff0648800c28185cd258a
ACF <alricfernandes11@gmail.com>
2023-06-21 16:41:28 -0400

    Adding express global variable and middleware

--- server/server.js
+++ server/server.js
@@ -1,2 +1,10 @@
+// Imports
 const express = require('express');
 const cors = require('cors');
+
+// Global variables
+const app = express();
+
+// Middleware
+app.use(cors())
+app.use(express.json());
