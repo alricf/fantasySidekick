9e68c84fc35fd9ffa130ff71eae62740c51757e6
ACF <alricfernandes11@gmail.com>
2023-06-22 13:24:02 -0400

    Adding mock data

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,40 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
+// Mock data
+const homeData = [
+  {
+    "season_name": "2017/18",
+    "total_points": 2186,
+    "rank": 156281
+  },
+  {
+    "season_name": "2018/19",
+    "total_points": 2091,
+    "rank": 920916
+  },
+  {
+    "season_name": "2019/20",
+    "total_points": 2122,
+    "rank": 739779
+  },
+  {
+    "season_name": "2020/21",
+    "total_points": 2212,
+    "rank": 829576
+  },
+  {
+    "season_name": "2021/22",
+    "total_points": 2217,
+    "rank": 1091914
+  },
+  {
+    "season_name": "2022/23",
+    "total_points": 2359,
+    "rank": 969228
+  }
+];
+
 function App() {
   // Hooks
 
