d9193a030ce6c7a82055a4b0b3fba0652c1a3dc4
ACF <alricfernandes11@gmail.com>
2023-06-26 12:42:51 -0400

    Connecting with back-end data and displaying in charts

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import Button from "./components/Button";
 
 Chart.register(CategoryScale);
 
-// Mock data
+/* Mock data
 const homeData = [
   {
     "season_name": "2017/18",
@@ -41,58 +41,71 @@ const homeData = [
     "rank": 969228
   }
 ];
+*/
 
 export default function App() {
-  // Hooks
 
-  // States
-  const [homeTotalPointsChartData, setHomeTotalPointsChartData] = useState({
-    labels: homeData.map((data) => data.season_name),
-    datasets: [
-      {
-        label: "Total Points",
-        data: homeData.map((data) => data.total_points),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0"
-        ],
-        borderColor: "black",
-        borderWidth: 2
-      }
-    ]
-  });
-
-  const [homeRankChartData, setHomeRankChartData] = useState({
-    labels: homeData.map((data) => data.season_name),
-    datasets: [
-      {
-        label: "Total Points",
-        data: homeData.map((data) => data.rank),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0"
-        ],
-        borderColor: "black",
-        borderWidth: 2
-      }
-    ]
-  });
-
+  const [homeData, setHomeData] = useState([]);
+  const [showChart, setShowChart] = useState(false);
   const [totalPointsChart, setTotalPointsChart] = useState(false);
   const [rankChart, setRankChart] = useState(false);
-
   const [managerId, setManagerId] = useState("");
+  // let showChart = false;
+  useEffect(() => {
+    // fetch(`http://localhost:8000/${managerId}`)
+    fetch(`http://localhost:8000/home-data`)
+      .then((res) => res.json())
+      .then((data) => setHomeData(data));
+  }, [showChart]);
+
+
+  const seasonNames = homeData.map((data) => data.season_name);
+  const totalPoints = homeData.map((data) => data.total_points);
+  const rank = homeData.map((data) => data.rank);
+
+  let homeTotalPointsChartData = {
+    labels: seasonNames,
+    datasets: [
+      {
+        label: "Total Points",
+        data: totalPoints,
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0"
+        ],
+        borderColor: "black",
+        borderWidth: 2
+      }
+    ]
+  };
+
+  let homeRankChartData = {
+    labels: seasonNames,
+    datasets: [
+      {
+        label: "Total Points",
+        data: rank,
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0"
+        ],
+        borderColor: "black",
+        borderWidth: 2
+      }
+    ]
+  };
 
   // Helper Functions
   const showTotalPointsChart = () => {
     setTotalPointsChart(true);
     setRankChart(false);
+    setShowChart(true);
   };
 
   const showRankChart = () => {
@@ -103,10 +116,12 @@ export default function App() {
   // Template
   return (
     <div>
-      <form class="flex justify-center my-5">
-        <label>Manager ID:</label>
+      <form className="flex justify-center my-5" method="post" action="http://localhost:8000/home-data">
+        <label htmlFor="home">Manager ID:</label>
         <input
+          id="home"
           type="text"
+          name="home"
           value={managerId}
           onChange={(e) => setManagerId(e.target.value)}
           className={"ml-5 border-solid border-2 border-black"}
@@ -115,6 +130,7 @@ export default function App() {
           type="submit"
           value="Submit"
           className={"border-solid border-black bg-yellow-300 border-2 ml-5"}
+          onClick={showTotalPointsChart}
         />
       </form>
       {/* Chart Logic */}
@@ -130,21 +146,18 @@ export default function App() {
           buttonText={"Season vs Rank"}
         />
       </div>
-      {(!totalPointsChart && !rankChart) &&
-        <LineChart
-          chartData={homeTotalPointsChartData}
-          text={"Season vs. Total Points"}
-        />}
       {totalPointsChart &&
         <LineChart
           chartData={homeTotalPointsChartData}
           text={"Season vs. Total Points"}
-        />}
+        />
+      }
       {rankChart &&
         <LineChart
           chartData={homeRankChartData}
           text={"Season vs. Rank"}
-        />}
+        />
+      }
     </div>
   );
 }
