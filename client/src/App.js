a6ded3846a2e596479bdaaf3f953a70296e2c3d0
ACF <alricfernandes11@gmail.com>
2023-06-26 12:44:10 -0400

    Removing mock data

--- client/src/App.js
+++ client/src/App.js
@@ -8,41 +8,6 @@ import Button from "./components/Button";
 
 Chart.register(CategoryScale);
 
-/* Mock data
-const homeData = [
-  {
-    "season_name": "2017/18",
-    "total_points": 2186,
-    "rank": 156281
-  },
-  {
-    "season_name": "2018/19",
-    "total_points": 2091,
-    "rank": 920916
-  },
-  {
-    "season_name": "2019/20",
-    "total_points": 2122,
-    "rank": 739779
-  },
-  {
-    "season_name": "2020/21",
-    "total_points": 2212,
-    "rank": 829576
-  },
-  {
-    "season_name": "2021/22",
-    "total_points": 2217,
-    "rank": 1091914
-  },
-  {
-    "season_name": "2022/23",
-    "total_points": 2359,
-    "rank": 969228
-  }
-];
-*/
-
 export default function App() {
 
   const [homeData, setHomeData] = useState([]);
