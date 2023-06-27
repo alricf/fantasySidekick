0a7353cf28a44872eaa65b7aba4effe32a540f96
ACF <alricfernandes11@gmail.com>
2023-06-27 11:56:11 -0400

    Setting up react router with Home component as root link

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 // Imports
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 
 export default function App() {
   // Template
   return (
     <>
-    <Home/>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
