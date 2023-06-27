1b919fae7ad27026f9e9241e3509e2f996d9ae27
ACF <alricfernandes11@gmail.com>
2023-06-27 12:12:56 -0400

    Styling nav bar list items

--- client/src/App.js
+++ client/src/App.js
@@ -10,12 +10,12 @@ export default function App() {
     <>
       <Router>
         <ul className="flex justify-center space-x-5">
-          <li>
+          <li className="border-black border-2 bg-sky-400 px-2">
             <Link to="/">Home</Link>
           </li>
-          <li>Chart</li>
-          <li>Top 100 Stats</li>
-          <li>Mini-League Stats</li>
+          <li className="border-black border-2 bg-sky-400 px-2">Chart</li>
+          <li className="border-black border-2 bg-sky-400 px-2">Top 100 Stats</li>
+          <li className="border-black border-2 bg-sky-400 px-2">Mini-League Stats</li>
         </ul>
         <Routes>
           <Route exact path="/" element={<Home />} />
