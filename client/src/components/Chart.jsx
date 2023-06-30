a23964c6b2c66f6c428b42001b38ea2cda5c3445
ACF <alricfernandes11@gmail.com>
2023-06-30 14:47:26 -0400

    Querying back-end express server for player statistic data

--- client/src/components/Chart.jsx
+++ client/src/components/Chart.jsx
@@ -1,7 +1,18 @@
 import React, { useState, useEffect } from "react";
+// const axios = require('axios');
+import axios from 'axios'
 
 export default function Chart() {
-  return(
+
+
+  useEffect(() => {
+    axios.get(`http://localhost:8000/chart`)
+      .then((res) => {
+        console.log(res.data);
+      });
+  }, []);
+
+  return (
     <>
     </>
   );
