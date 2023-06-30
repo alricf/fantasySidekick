605e0c35d618324e5d51cc88a62fc6ae79be1f62
ACF <alricfernandes11@gmail.com>
2023-06-30 14:58:28 -0400

    Adding Chart link

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -5,14 +5,16 @@ export default function NavBar() {
 
   return (
     <>
-    <ul className="flex justify-center space-x-5 mt-5">
-      <li className="border-black border-2 bg-sky-400 px-2">
-        <Link to="/">Home</Link>
-      </li>
-      <li className="border-black border-2 bg-sky-400 px-2">Chart</li>
-      <li className="border-black border-2 bg-sky-400 px-2">Top 100 Stats</li>
-      <li className="border-black border-2 bg-sky-400 px-2">Mini-League Stats</li>
-    </ul>
+      <ul className="flex justify-center space-x-5 mt-5">
+        <li className="border-black border-2 bg-sky-400 px-2">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="border-black border-2 bg-sky-400 px-2">
+          <Link to="/chart">Chart</Link>
+        </li>
+        <li className="border-black border-2 bg-sky-400 px-2">Top 100 Stats</li>
+        <li className="border-black border-2 bg-sky-400 px-2">Mini-League Stats</li>
+      </ul>
     </>
   );
 };
