c9bfdd904a4aa07aa0eb6219844c35c9f74fba5e
ACF <alricfernandes11@gmail.com>
2023-07-04 14:01:48 -0400

    Accessing title prop

--- client/src/components/LineChart.jsx
+++ client/src/components/LineChart.jsx
@@ -2,11 +2,11 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
 
-export default function LineChart({ chartData, text }) {
+export default function LineChart({ chartData, text, title }) {
   // Template
   return (
     <div className="chart-container">
-      <h2 style={{ textAlign: "center" }}>Manager Chart</h2>
+      <h2 style={{ textAlign: "center" }}>{title}</h2>
       <Line
         data={chartData}
         options={{
